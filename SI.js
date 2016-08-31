e80820d66b8589f58f4b38e29de3b557a8fc78c6
Alex Hanson <alexhanson@users.noreply.github.com>
2016-08-31 00:01:23 -0700

    Make a big refactor for code cleanliness and concision.

--- SI.js
+++ SI.js
@@ -1,75 +1,105 @@
 var SI = {
-	disableBrowsingHistoryNow: function() {
+	// Public methods
+
+	start: function() {
 		console.debug(chrome.i18n.getMessage("checking_for_initial_session"));
+
+		getSessionId().then(
+			function(sessionId) {
+				console.debug(chrome.i18n.getMessage("existing_session_found"));
+				return this._recordSessionId(sessionId);
+			}.bind(this), 
+			function() {
+				console.info(chrome.i18n.getMessage("no_session_found"));
+			}.bind(this)
+		).then(
+			function() {
+				chrome.cookies.onChanged.addListener(this._onCookieChange.bind(this));
+				console.info(chrome.i18n.getMessage("cookie_monitoring_enabled"));
+			}.bind(this)
+		);
+	},
+
+
+	// State
+
+	_sessionId: null,
+
+
+	// Private methods
+
+	_onCookieChange: function(changeInfo) {
+		if (!changeInfo.removed) {
+			var cookie = changeInfo.cookie;
+			if (cookie.domain === ".amazon.com" && cookie.name === "session-id") {
+				this._recordSessionId(cookie.value);
+			}
+		}
+	},
+
+	_recordSessionId(sessionId) {
+		if (sessionId !== this._sessionId) {
+			if (this._sessionId) {
+				console.debug(chrome.i18n.getMessage("new_session_found"));
+			}
+
+			this._sessionId = sessionId;
+			return disableBrowsingHistory();
+		} else {
+			return Promise.resolve();
+		}
+	}
+};
+
+
+function postForm(url, formData) {
+	return new Promise(function(resolve, reject) {
+		var request = new XMLHttpRequest();
+
+		request.onreadystatechange = function() {
+			if (request.readyState === XMLHttpRequest.DONE) {
+				if (request.status === 200) {
+					resolve();
+				} else {
+					reject();
+				}
+			}
+		};
+
+		request.open("POST", url);
+		request.send(formData);
+	});
+}
+
+function disableBrowsingHistory() {
+	console.debug(chrome.i18n.getMessage("disabling_browsing_history"));
+
+	var amazonEndpoint = "https://www.amazon.com/gp/mobile/ybh/handlers/click-stream.html";
+
+	var formData = new FormData();
+	formData.append("action", "disable");
+
+	return postForm(amazonEndpoint, formData).then(
+		function() {
+			console.info(chrome.i18n.getMessage("disable_browsing_success"));
+		}.bind(this),
+		function() {
+			console.error(chrome.i18n.getMessage("disable_browsing_error"));
+		}.bind(this)
+	);
+}
+
+function getSessionId() {
+	return new Promise(function(resolve, reject) {
 		chrome.cookies.get({
 			url: "http://www.amazon.com",
 			name: "session-id"
 		}, function(cookie) {
 			if (cookie) {
-				this._sessionId = cookie.value;
-				console.debug(chrome.i18n.getMessage("existing_session_found"));
-				this._disableBrowsingHistory();
+				resolve(cookie.value);
 			} else {
-				console.info(chrome.i18n.getMessage("no_session_found"));
+				reject(new Error("Could not retrieve current Amazon session ID."));
 			}
-		}.bind(this));
-	},
-
-	disableBrowsingHistoryWhenSessionIdChanges: function() {
-		console.info(chrome.i18n.getMessage("cookie_monitoring_enabled"));
-		chrome.cookies.onChanged.addListener(this._disableBrowsingHistoryWhenSessionIdChanges.bind(this));
-	},
-
-
-	// Internal
-
-	_sessionId: null,
-	_suspendAutoDisable: false,
-
-	_disableBrowsingHistoryWhenSessionIdChanges: function(changeInfo) {
-		if (this._suspendAutoDisable) {
-			return;
-		}
-
-		if (!changeInfo.removed) {
-			var cookie = changeInfo.cookie;
-			if (cookie.domain === ".amazon.com" && cookie.name === "session-id") {
-				var newSessionId = cookie.value;
-				if (this._sessionId !== newSessionId) {
-					this._sessionId = newSessionId;
-					console.debug(chrome.i18n.getMessage("new_session_found"));
-					this._disableBrowsingHistory();
-				}
-			}
-		}
-	},
-
-	_disableBrowsingHistory: function() {
-		this._suspendAutoDisable = true;
-
-		console.debug(chrome.i18n.getMessage("disabling_browsing_history"));
-
-		var formData = new FormData();
-		formData.append("action", "disable");
-
-		var request = new XMLHttpRequest();
-
-		request.onload = function(e) {
-			if (e.target.status === 200) {
-				console.info(chrome.i18n.getMessage("disable_browsing_success"));
-			} else {
-				console.error(chrome.i18n.getMessage("disable_browsing_error"));
-			}
-
-			this._suspendAutoDisable = false;
-		}.bind(this);
-
-		request.onerror = function(e) {
-			console.error(chrome.i18n.getMessage("disable_browsing_error"));
-			this._suspendAutoDisable = false;
-		}.bind(this);
-
-		request.open("POST", "https://www.amazon.com/gp/mobile/ybh/handlers/click-stream.html");
-		request.send(formData);
-	}
-};
+		});
+	});
+}
--- background.js
+++ background.js
@@ -1,5 +1,1 @@
-// If an Amazon session exists, disable Amazon browsing history now.
-SI.disableBrowsingHistoryNow();
-
-// Disable Amazon browsing history any time the session is updated.
-SI.disableBrowsingHistoryWhenSessionIdChanges();
+SI.start();
