22d1cbf82d2372a04b5ab20d004a2d029ba3c216
Alex Hanson <alexhanson@users.noreply.github.com>
2016-09-17 18:54:56 -0700

    Exclude all hidden (dot-prefixed) files when packaging extension.

--- package.sh
+++ package.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 pushd $(dirname "${0}") > /dev/null
 find . -depth 1 \
-	-not -name '.git' \
-	-not -name '.gitignore' \
+	-not -name '.*' \
 	-not -name 'package.sh' \
 	-not -name 'shop-incognito.zip' \
 	-print | zip -r shop-incognito.zip -@
