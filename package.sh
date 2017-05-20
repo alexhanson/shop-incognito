b34f852413f8cb03349de279a9c49c550ad48189
Alex Hanson <public+shop-incognito@alexhanson.org>
2017-05-20 08:06:53 -0700

    Update package.sh to ignore dotfiles in subdirectories and start fresh every time.

--- package.sh
+++ package.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 pushd $(dirname "${0}") > /dev/null
-find . -depth 1 \
-	-not -name '.*' \
-	-not -name 'package.sh' \
-	-not -name 'shop-incognito.zip' \
-	-print | zip -r shop-incognito.zip -@
+
+OUTPUT="shop-incognito.zip"
+rm "$OUTPUT"
+
+find . \
+	\( \
+			-type d \
+			-name '.git' \
+			-prune \
+		-o \
+			-type f \
+			-not -name '.*' \
+			-not -name 'package.sh' \
+			-not -name 'shop-incognito.zip' \
+			-print \
+	\) | zip -r "$OUTPUT" -@
